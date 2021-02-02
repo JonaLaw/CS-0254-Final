d6cdcb791899d24d852dd15a41be79fe397a57c2
JonaLaw <33699564+JonaLaw@users.noreply.github.com>
2021-02-01 22:10:36 -0500

    Update test.clj

--- test.clj
+++ test.clj
@@ -14,3 +14,9 @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
 ;; <=
+
+;; **
+;;; This code defines and runs a genetic programming system on the problem
+;;; of finding a function that fits a particular set of [x y] pairs.
+;;; The evolve function will try to find the function for the data-set labled target-data.
+;; **
