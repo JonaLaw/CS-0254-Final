bd398759ca14017ae7f54f9c901037bfcf601186
JonaLaw <33699564+JonaLaw@users.noreply.github.com>
2021-02-01 22:07:47 -0500

    Update test.clj

--- test.clj
+++ test.clj
@@ -5,7 +5,6 @@
 ;;;
 ;; **
 
-
 ;; @@
 (ns worksheets.simple-symbolic-regression
   (:require [gorilla-plot.core :as plot]
@@ -15,9 +14,3 @@
 ;; =>
 ;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
 ;; <=
-
-;; **
-;;; This code defines and runs a genetic programming system on the problem
-;;; of finding a function that fits a particular set of [x y] pairs.
-;;; The evolve function will try to find the function for the data-set labled target-data.
-;; **
